03ee90b008b743e0e63be362697619e33d2b068b
KoalaGuy-100 <botoplol561@gmail.com>
2025-01-23 12:37:28 +0800

    Update FleeTheFacility.lua

--- FleeTheFacility.lua
+++ FleeTheFacility.lua
@@ -1,60 +1,34 @@
-local KHLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/KoalaGuy-100/Koala-Hacks/refs/heads/main/KHLib/Vmj1.lua"))()
-local UI = KHLib.new(nil, "KH_FTF")
-UI.DisplayOrder = math.huge
+-- # CHANGE TO loadstring(game:HttpGet("https://raw.githubusercontent.com/KoalaGuy-100/Koala-Hacks/refs/heads/main/KHLib/Vmj1.lua"))()
+local KHLib = require(script.Parent.Main)
+local UI = KHLib.new(nil, "KHui_FTF")
 
-local ESP_Players = false
-local ESP_PodsExits = false
-local ESP_Computers = false
+local UserInputService = game:GetService("UserInputService")
 
-local EspTab = UI.newtab("ESP", "ESPTab")
-EspTab:CreateInput("Activate", "Toggle Player ESP.").Activate.MouseButton1Down:Connect(function()
-	if ESP_Players == true then
-		ESP_Players = false
-	else
-		ESP_Players = true
-	end
-end)
-EspTab:CreateInput("Activate", "Toggle Pods & Exits ESP.").Activate.MouseButton1Down:Connect(function()
-	if ESP_PodsExits == true then
-		ESP_PodsExits = false
-	else
-		ESP_PodsExits = true
-	end
-end)
-EspTab:CreateInput("Activate", "Toggle Computers ESP.").Activate.MouseButton1Down:Connect(function()
-	if ESP_Computers == true then
-		ESP_Computers = false
-	else
-		ESP_Computers = true
-	end
-end)
+local ESPTab = UI.newtab("ESP's", "ESPTab")
 
-local AntiPCError = false
-local FullBright = false
+local ESP_Players = ESPTab:CreateInput("Toggle", "ESP Players.", "ESP_Players").Input
+local ESP_Computers = ESPTab:CreateInput("Toggle", "ESP Computers.", "ESP_Computers").Input
+local ESP_Exits = ESPTab:CreateInput("Toggle", "ESP Exits.", "ESP_Exits").Input
+local ESP_Pods = ESPTab:CreateInput("Toggle", "ESP Freeze Pods.", "ESP_Pods").Input
+local ESP_Refresh = ESPTab:CreateInput("Activate", "Refresh ESP's.", "ESP_Refresh").Activate
 
-local ExtraTab = UI.newtab("Extra", "ExtraTab")
-ExtraTab:CreateInput("Activate", "Toggle Anti PC Error.").Activate.MouseButton1Down:Connect(function()
-	if AntiPCError == true then
-		AntiPCError = false
-	else
-		AntiPCError = true
-	end
-end)
-ExtraTab:CreateInput("Activate", "Toggle FullBright.").Activate.MouseButton1Down:Connect(function()
-	if FullBright == true then
-		FullBright = false
-	else
-		FullBright = true
-	end
-end)
-local Speed = ExtraTab:CreateInput("TextBox", "Input Walk Speed.")
+local ExtraTab = UI.newtab("Extras", "ExtraTab")
 
-while wait() do
-	-- ESP PLAYERS
+local AntiPCError = ExtraTab:CreateInput("Toggle", "Anti Computer Error.", "AntiPCError").Input
+local FullBright = ExtraTab:CreateInput("Toggle", "Full Bright.", "FullBright").Input
+local PlrSpeed = ExtraTab:CreateInput("TextBox", "Player WalkSpeed.", "PlrSpeed").TextBox
+local PlrJumpPower = ExtraTab:CreateInput("TextBox", "Player Jump Power.", "PlrJumpPower").TextBox
+
+local OnRound = Instance.new("BoolValue", UI.Instance)
+
+-- # MODIFY UI
+UI.Title.Text = "Koala Scripts - Flee The Facility Script"
+
+-- # Check ESP's Function
+function CheckPlrESP()
 	for i, v in pairs(game.Players:GetPlayers()) do
-		wait()
 		if v.Character ~= nil and v ~= game.Players.LocalPlayer then
-			if not v.Character:FindFirstChild("PlayerESP_KH") and ESP_Players == true then
+			if not v.Character:FindFirstChild("PlayerESP_KH") and ESP_Players.Value == true then
 				local PlayerESP = Instance.new("Highlight", v.Character)
 				PlayerESP.Name = "PlayerESP_KH"
 				PlayerESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
@@ -62,7 +36,7 @@ while wait() do
 				if v.Character:FindFirstChild("BeastPowers") then
 					PlayerESP.FillColor = Color3.fromRGB(255, 0, 0)
 				end
-			elseif v.Character:FindFirstChild("PlayerESP_KH") and ESP_Players == true then
+			elseif v.Character:FindFirstChild("PlayerESP_KH") and ESP_Players.Value == true then
 				v.Character.PlayerESP_KH.FillColor = Color3.fromRGB(0, 255, 0)
 				if v.Character:FindFirstChild("BeastPowers") then
 					v.Character.PlayerESP_KH.FillColor = Color3.fromRGB(255, 0, 0)
@@ -72,42 +46,104 @@ while wait() do
 			end
 		end
 	end
-	
-	-- ESP PODS & EXITS
+end
+function CheckPodsESP()
 	for i, v in pairs(game.Workspace:GetDescendants()) do
-		wait()
 		if v.Name == "FreezePod" then
-			if not v:FindFirstChild("FreezePodESP_KH") and ESP_PodsExits == true then
+			if not v:FindFirstChild("FreezePodESP_KH") and ESP_Pods.Value == true then
 				local PodESP = Instance.new("Highlight", v)
 				PodESP.Name = "FreezePodESP_KH"
 				PodESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
 				PodESP.FillColor = Color3.fromRGB(0, 150, 255)
-			elseif v:FindFirstChild("FreezePodESP_KH") and ESP_PodsExits == false then
+			elseif v:FindFirstChild("FreezePodESP_KH") and ESP_Pods.Value == false then
 				v.FreezePodESP_KH:Destroy()
 			end
 		end
+	end
+end
+function CheckExitsESP()
+	for i, v in pairs(game.Workspace:GetDescendants()) do
 		if v.Name == "ExitDoor" then
-			if not v:FindFirstChild("ExitDoorESP_KH") and ESP_PodsExits == true then
+			if not v:FindFirstChild("ExitDoorESP_KH") and ESP_Exits.Value == true then
 				local ExitESP = Instance.new("Highlight", v)
 				ExitESP.Name = "ExitDoorESP_KH"
 				ExitESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
 				ExitESP.FillColor = Color3.fromRGB(0, 255, 150)
-			elseif v:FindFirstChild("ExitDoorESP_KH") and ESP_PodsExits == false then
+			elseif v:FindFirstChild("ExitDoorESP_KH") and ESP_Exits.Value == false then
 				v.ExitDoorESP_KH:Destroy()
 			end
 		end
 	end
-	
-	-- COMPUTERS
+end
+function OnRoundCheck()
+	CheckPodsESP()
+	CheckExitsESP()
+	CheckPlrESP()
+end
+
+-- # TICK FUNCTION
+OnRound.Changed:Connect(function()
+	OnRoundCheck()
+	task.wait(5)
+	OnRoundCheck()
+	task.wait(15)
+	OnRoundCheck()
+end)
+
+-- • PLR ESP
+game.Players.PlayerAdded:Connect(function()
+	CheckPlrESP()
+end)
+game.Players.PlayerRemoving:Connect(function()
+	CheckPlrESP()
+end)
+ESP_Players.Changed:Connect(function()
+	CheckPlrESP()
+end)
+
+-- • PODS ESP
+ESP_Pods.Changed:Connect(function()
+	CheckPodsESP()
+end)
+
+-- • EXITS ESP
+ESP_Exits.Changed:Connect(function()
+	CheckExitsESP()
+end)
+
+-- • REFRESH ESP's
+ESP_Refresh.MouseButton1Down:Connect(function()
+	OnRoundCheck()
+end)
+
+-- # TOGGLE KEY
+function ToggleUI()
+	if UI.Instance.Main.Visible == false then
+		UI.Instance.Main.Visible = true
+		UI.Instance.Open.Visible = false
+	else
+		UI.Instance.Main.Visible = false
+		UI.Instance.Open.Visible = true
+	end
+end
+UserInputService.InputBegan:Connect(function(Input, GameProcessed)
+	if Input.KeyCode == Enum.KeyCode.LeftAlt and not GameProcessed then
+		ToggleUI()
+	end
+end)
+
+while wait() do
+	-- # ESP COMPUTERS
+	local GotComputers = 0
 	for i, v in pairs(game.Workspace:GetDescendants()) do
-		wait()
+		GotComputers += 1
 		if v.Name == "ComputerTable" then
-			if not v:FindFirstChild("ComputerTableESP_KH") and ESP_Computers == true then
+			if not v:FindFirstChild("ComputerTableESP_KH") and ESP_Computers.Value == true then
 				local ComputerTableESP = Instance.new("Highlight", v)
 				ComputerTableESP.Name = "ComputerTableESP_KH"
 				ComputerTableESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
 				ComputerTableESP.FillColor = v:WaitForChild("Screen").Color
-			elseif ESP_Computers == true then
+			elseif ESP_Computers.Value == true then
 				v.ComputerTableESP_KH.FillColor = v:WaitForChild("Screen").Color
 			elseif  v:FindFirstChild("ComputerTableESP_KH") then
 				v.ComputerTableESP_KH:Destroy()
@@ -115,23 +151,38 @@ while wait() do
 		end
 	end
 	
-	-- ANTI PC ERROR
-	if AntiPCError == true then
+	-- # ANTI PC ERROR
+	if AntiPCError.Value then
 		game.ReplicatedStorage.RemoteEvent:FireServer("SetPlayerMinigameResult", true)
 	end
 	
-	-- FULL BRIGHT
-	if FullBright == true and not game.Players.LocalPlayer.Character:FindFirstChild("KH_FullBright") then
-		local NewLight = Instance.new("PointLight", game.Players.LocalPlayer.Character)
-		NewLight.Name = "KH_FullBright"
-		NewLight.Range = math.huge
-		NewLight.Brightness = 2.5
-	elseif FullBright == false and game.Players.LocalPlayer.Character:FindFirstChild("KH_FullBright") then
-		game.Players.LocalPlayer.Character.KH_FullBright:Destroy()
+	-- # FULL BRIGHT
+	if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+		if FullBright.Value == true and not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("KH_FullBright") then
+			local NewLight = Instance.new("PointLight", game.Players.LocalPlayer.Character.HumanoidRootPart)
+			NewLight.Name = "KH_FullBright"
+			NewLight.Range = math.huge
+			NewLight.Brightness = 2.5
+		elseif FullBright.value == false and game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("KH_FullBright") then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.KH_FullBright:Destroy()
+		end
 	end
 	
-	-- SPEED
-	if tonumber(Speed.TextBox.Text) ~= nil then
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Speed.TextBox.Text
+	-- # PLR SPEED & PLR JUMP POWER
+	if tonumber(PlrSpeed.Text) and game.Players.LocalPlayer.Character then
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = tonumber(PlrSpeed.Text)
+	elseif tonumber(PlrJumpPower.Text)  and game.Players.LocalPlayer.Character then
+		game.Players.LocalPlayer.Character.Humanoid.UseJumpPower = true
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = tonumber(PlrJumpPower.Text)
+	elseif game.Players.LocalPlayer.Character then
+		game.Players.LocalPlayer.Character.Humanoid.UseJumpPower = true
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+	end
+	
+	-- # ON ROUND DETECTOR
+	if GotComputers > 0 then
+		OnRound.Value = true
+	else
+		OnRound.Value = false
 	end
 end
